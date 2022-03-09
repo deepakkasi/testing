49aa6f63e6e32bb667da1e2f4d2d484ccbae5fec
deepak <deepak2000kasi@gmail.com>
2022-03-09 13:54:35 +0530

    cleared the bug

--- sum.c
+++ sum.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main();
-int a =5 ,b=10,sum;
-sum= a + b;
-printf("sum of the value is =" sum ,&d);
+int main()
+{
+	int sum = 7 + 3;
+    printf("sum of the value is =10");
+}
+
